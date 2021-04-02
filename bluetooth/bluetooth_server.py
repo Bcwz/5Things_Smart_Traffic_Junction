ded66551e900c207d4acc2c4b49287d636a399a1
yipeyayeah <yipeyayeah@gmail.com>
2021-04-03 03:46:50 +0800

    Added bluetooth server file

---
+++ bluetooth/bluetooth_server.py
@@ -0,0 +1,39 @@
+from bluedot.btcomm import BluetoothServer
+from time import sleep
+from signal import pause
+from datetime import datetime
+
+def data_received(data):
+    # Get current time
+    now = datetime.now().strftime("%H:%M:%S.%f")
+    current_time = datetime.strptime(now,"%H:%M:%S.%f")
+    # Convert the payload time to a datetime object
+    payload_time = datetime.strptime(data[0:12], '%H:%M:%S.%f')
+    latency=current_time-payload_time
+    server.send("Data received. Latency: {}".format(latency))
+
+def client_connected():
+    print("Client connected")
+
+def client_disconnected():
+    print("Client disconnected")
+
+print("Bluetooth server is starting.")
+server = BluetoothServer(
+    data_received,
+    auto_start = False,
+    when_client_connects = client_connected,
+    when_client_disconnects = client_disconnected)
+
+server.start()
+print("MAC Address: ", server.server_address)
+print("Waiting for connection")
+
+try:
+    pause()
+except KeyboardInterrupt as e:
+    print("cancelled by user")
+finally:
+    print("stopping")
+    server.stop()
+print("stopped")
